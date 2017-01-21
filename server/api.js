ed887954767b5c9d8f40cd5ce868d5b3f14c810a
Chris Hampu <chrishampu@outlook.com>
2017-01-21 02:19:19 -0800

    Add route for extracting a schema
    Add function which can pull tables/columns/relations from a database
    Rework some utility functions to be more generic
    Resolves #NEXT-19

--- server/api.js
+++ server/api.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 const koaRouter = require('koa-router');
 
 const router = koaRouter();
@@ -5,26 +6,60 @@ const router = koaRouter();
 const mysql = require('./mysql');
 
 /**
+ * Returns a boolean with whether the given object contains
+ * all the fields necessary to facilitate a MySQL connection
+ */
+function verifyProjectFields(fields) {
+
+  if (!fields ||
+    !fields.connection_info ||
+    !fields.connection_info.host ||
+    !fields.connection_info.user ||
+    !fields.connection_info.password ||
+    !fields.connection_info.database) {
+    return false;
+  }
+
+  return true;
+}
+
+/**
+ * This POST request takes in MySQL connection parameters, connects
+ * to the MySQL server, and will validate that the parameters are correct
+ */
+router.post('/mysql/extract', async (ctx) => {
+
+  const project = ctx.request.fields;
+
+  if (!verifyProjectFields(project)) {
+
+    ctx.body = 'Required parameters are missing';
+    return;
+  }
+
+  ctx.body = await mysql.extractSchema(project);
+});
+
+/**
  * This POST request takes in MySQL connection parameters, connects
  * to the MySQL server, and will validate that the parameters are correct
  */
 router.post('/mysql/testconnection', async (ctx) => {
 
-  if (!ctx.request.fields ||
-    !ctx.request.fields.host ||
-    !ctx.request.fields.user ||
-    !ctx.request.fields.password) {
+  const project = ctx.request.fields;
 
-    ctx.body = 'Required parameters are missing'; // eslint-disable-line no-param-reassign
+  if (!verifyProjectFields(project)) {
+
+    ctx.body = 'Required parameters are missing';
     return;
   }
 
-  const valid = await mysql.verifyConnectionParameters(ctx.request.fields);
+  const valid = await mysql.verifyConnectionParameters(project);
 
   if (valid) {
-    ctx.body = 'Connected'; // eslint-disable-line no-param-reassign
+    ctx.body = 'Connected';
   } else {
-    ctx.body = 'Failed to connect'; // eslint-disable-line no-param-reassign
+    ctx.body = 'Failed to connect';
   }
 });
 
--- server/mysql.js
+++ server/mysql.js
@@ -1,24 +1,156 @@
+/* eslint-disable camelcase, no-restricted-syntax */
 const mysql = require('mysql');
+
+/**
+ * Will perform a MySQL connection for the given project and connection info
+ * If the connection fails, an error object will be returned. If succesful, the connection
+ * object is returned. The sucessful connection must be destroyed after it is no longer needed
+ */
+async function getConnection(project) {
+
+  return new Promise((resolve, reject) => {
+
+    const connection = mysql.createConnection(project.connection_info);
+
+    connection.connect((err) => {
+
+      if (err) {
+        connection.destroy();
+        reject(err);
+      } else {
+        resolve(connection);
+      }
+    });
+  });
+}
 
 /**
  * Returns a boolean of whether the passed-in mysql connection
  * info is valid or not
  */
-async function verifyConnectionParameters(parameters) {
+async function verifyConnectionParameters(project) {
 
-  return new Promise((resolve) => {
+  let connection = null;
 
-    const connection = mysql.createConnection(parameters);
+  try {
+    connection = await getConnection(project);
+  } catch (err) {
+    return false;
+  }
 
-    connection.connect((err) => {
+  if (connection) {
+    connection.destroy();
+  }
 
-      resolve(!err);
+  return true;
+}
 
-      connection.destroy();
+/**
+ * Utility that will return a promise which will resolve or reject depending
+ * on the callback result from a SQL query using the provided connection and
+ * query string
+ */
+async function promisifyQuery(connection, query) {
+
+  return new Promise((resolve, reject) => {
+
+    connection.query(query, (err, results, fields) => {
+
+      if (err) {
+        reject(err);
+      } else {
+        resolve({ results, fields });
+      }
     });
   });
 }
 
+/**
+ * This function will perform database analysis and
+ * return an object which follows the documented schema json structure
+ */
+async function extractSchema(project) {
+
+  let connection = null;
+
+  try {
+    connection = await getConnection(project);
+  } catch (err) {
+    return {
+      error: err,
+    };
+  }
+
+  const tables = [];
+  const stats = {
+    table_count: 0,
+    column_count: 0,
+    relationship_count: 0,
+  };
+
+  const table_query = await promisifyQuery(connection, 'show tables');
+
+  const table_field_name = table_query.fields[0].name;
+
+  for (const row of table_query.results) {
+
+    const table_name = row[table_field_name];
+    const columns = [];
+    const relationships = [];
+
+    // Given a table name, we request a data structure that represents all of the columns in the table
+    const column_query = await promisifyQuery(connection, `show columns from ${table_name}`); // eslint-disable-line no-await-in-loop
+
+    for (const col of column_query.results) {
+
+      const attributes = [];
+
+      if (col.Key === 'PRI') {
+        attributes.push('PK');
+      }
+
+      columns.push({
+        column_id: columns.length,
+        column_name: col.Field,
+        type: col.Type,
+        attributes,
+      });
+
+      stats.column_count += 1;
+    }
+
+    // Here we query the information_schema table for constraints given the table name
+    const relation_query = await promisifyQuery(connection, // eslint-disable-line no-await-in-loop
+      `select table_name, column_name, constraint_name, referenced_table_name,
+      referenced_column_name from information_schema.key_column_usage where
+      referenced_table_schema = '${project.connection_info.database}' 
+      and referenced_table_name = '${table_name}'`);
+
+    for (const relation of relation_query.results) {
+
+      relation.type = 'FK';
+
+      relationships.push(relation);
+
+      stats.relationship_count += 1;
+    }
+
+    tables.push({
+      table_id: tables.length,
+      table_name,
+      columns,
+      relationships,
+    });
+
+    stats.table_count += 1;
+  }
+
+  connection.destroy();
+
+  return { project, tables, stats };
+}
+
 module.exports = {
   verifyConnectionParameters,
+  extractSchema,
 };
