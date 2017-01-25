a146e49c5e3541e8a15398e371c18d10c0f82c08
Chris Hampu <chrishampu@outlook.com>
2017-01-24 17:43:04 -0800

    Add state management

---
+++ public/js/main.js
@@ -0,0 +1,3 @@
+/* eslint-disable no-unused-vars, no-undef */
+// Create a global state store
+const store = createStore();
---
+++ public/js/state.js
@@ -0,0 +1,52 @@
+/* eslint-disable no-unused-vars */
+/**
+ * This implementation is based loosely off of the Redux design paradigm
+ * The difference is that we give the store a function which can directly
+ * alter the state before triggering the listeners rather than relying
+ * on combining reducers and actions
+ *
+ * This will return an object with a few functions for managing global
+ * application state as well as subscribers to trigger when that state changes
+ */
+function createStore() {
+
+  // Sstate tree
+  let currentState = {};
+
+  // An array of functions to invoke when state is changed; observables
+  const currentListeners = [];
+
+  // Return the state of this store
+  function getState() {
+    return currentState;
+  }
+
+  // Will update the state and trigger all listener callbacks
+  function setState(newState) {
+    currentState = Object.assign({}, currentState, newState);
+
+    // Copy the listeners to avoid mutation during iteration
+    const listeners = currentListeners;
+
+    for (let i = 0; i < listeners.length; i++) { // eslint-disable-line no-plusplus
+
+      // Invoke the listener function
+      listeners[i]();
+    }
+  }
+
+  // Will add the listener function to a list of functions that will trigger whenever state changes
+  function subscribe(listener) {
+    if (typeof listener !== 'function') {
+      throw new Error('subscribe: listener is not a valid function');
+    }
+
+    currentListeners.push(listener);
+  }
+
+  return {
+    getState,
+    setState,
+    subscribe,
+  };
+}
